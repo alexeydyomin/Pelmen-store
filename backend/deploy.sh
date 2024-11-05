4f8481f6679f0d87fd73c7d4cb6f92f066a8a48f
std-030-35 std-030-35 <std-030-35@praktikum-services.ru>
2024-11-05 18:13:02 +0000

    Update 3 files
    - /backend/deploy.sh
    - /backend/.gitlab-ci.yml
    - /frontend/.gitlab-ci.yml

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,6 +4,7 @@ sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY
 sudo docker network create -d bridge momo_network || true
 sudo docker rm -f momo-backend || true
 sudo docker run -d --name momo-backend \
+     -p 8081:8081 \
      --network=momo_network \
      --restart=always \
      "${CI_REGISTRY_IMAGE}"/momo-backend:$VERSION
