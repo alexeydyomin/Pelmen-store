79e858f82f1d8b9574596078695bfb5c36afcfba
std-030-35 std-030-35 <std-030-35@praktikum-services.ru>
2024-11-12 13:46:21 +0000

    Update 6 files
    - /frontend/default.conf
    - /frontend/deploy.sh
    - /frontend/.gitlab-ci.yml
    - /frontend/Dockerfile
    - /backend/.gitlab-ci.yml
    - /backend/deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,9 +2,9 @@
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge momo_network || true
-sudo docker rm -f momo-backend || true
-sudo docker run -d --name momo-backend \
+sudo docker rm -f backend || true
+sudo docker run -d --name backend \
      -p 8081:8081 \
      --network=momo_network \
      --restart=always \
-     "${CI_REGISTRY_IMAGE}"/momo-backend:$VERSION
+     "${CI_REGISTRY_IMAGE}"/backend:$VERSION
--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,9 +2,9 @@
 set -xe
 sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 sudo docker network create -d bridge momo_network || true
-sudo docker rm -f momo-frontend || true
-sudo docker run -d --name momo-frontend \
+sudo docker rm -f frontend || true
+sudo docker run -d --name frontend \
      -p 80:80 \
      --network=momo_network \
      --restart=always \
-     "${CI_REGISTRY_IMAGE}"/momo-frontend:$VERSION
+     "${CI_REGISTRY_IMAGE}"/frontend:$VERSION
